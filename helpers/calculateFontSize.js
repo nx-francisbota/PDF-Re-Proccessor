f8065e90464e212b4abd961c7d6bcd0102439218
chavocito <102731139+nx-francisbota@users.noreply.github.com>
2024-03-17 18:38:34 +0000

    update font size calculation helper

--- helpers/calculateFontSize.js
+++ helpers/calculateFontSize.js
@@ -1,18 +1,47 @@
-const { Logger } = require('../helpers/logger');
+const { Logger } = require('../utils/logger');
+
+
 
 module.exports = {
-    calculateFontSize : (text) => {
+    calculateFontSize : (size, text) => {
         let fontSize;
         const charLength = text.length
-        if (charLength >= 1 && charLength <= 5) {
-            fontSize = 60;
-        } else if (charLength >= 6 && charLength <= 10) {
-            fontSize = 50;
-        } else if (charLength >= 11 && charLength <= 15) {
-            fontSize = 40;
-        } else {
-            Logger.warn("Text length is outside the defined ranges.")
-            fontSize = 50
+        switch(size) {
+            case "250":
+                if (charLength >= 1 && charLength <= 5) {
+                    fontSize = 90;
+                } else if (charLength >= 6 && charLength <= 10) {
+                    fontSize = 80;
+                } else if (charLength >= 11 && charLength <= 15) {
+                    fontSize = 40;
+                } else {
+                    Logger.warn("Text length is outside the defined ranges.")
+                    fontSize = 40
+                }
+                break;
+            case "400":
+                if (charLength >= 1 && charLength <= 5) {
+                    fontSize = 100;
+                } else if (charLength >= 6 && charLength <= 10) {
+                    fontSize = 90;
+                } else if (charLength >= 11 && charLength <= 15) {
+                    fontSize = 65;
+                } else {
+                    Logger.warn("Text length is outside the defined ranges.")
+                    fontSize = 40
+                }
+                break;
+            case "675":
+                if (charLength >= 1 && charLength <= 5) {
+                    fontSize = 120;
+                } else if (charLength >= 6 && charLength <= 10) {
+                    fontSize = 110;
+                } else if (charLength >= 11 && charLength <= 15) {
+                    fontSize = 75;
+                } else {
+                    Logger.warn("Text length is outside the defined ranges.")
+                    fontSize = 40
+                }
         }
         return fontSize;
     }
