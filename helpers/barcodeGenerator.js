5aa6854375eb04bacc411fc03cf8f756f0785767
chavocito <102731139+nx-francisbota@users.noreply.github.com>
2024-03-18 17:49:22 +0000

    add error handling

--- helpers/barcodeGenerator.js
+++ helpers/barcodeGenerator.js
@@ -1,4 +1,5 @@
 const bwipjs = require("bwip-js");
+const { logger } = require('../utils/logger');
 const { pdfConstants } = require('../constants/constants');
 
 const generateBarcodeSVGBuffer = async (prop) => {
@@ -12,33 +13,39 @@ const generateBarcodeSVGBuffer = async (prop) => {
 }
 
 async function generatePdfWithBarcode(data, pdfDoc) {
-    const barcodeProdData = await generateBarcodeSVGBuffer(data.productNumber);
-    const barcodeOrderData = await generateBarcodeSVGBuffer(data.orderNumber);
+    const { productNumber, orderNumber } = data;
+    const barcodeProdData = await generateBarcodeSVGBuffer(productNumber);
+    const barcodeOrderData = await generateBarcodeSVGBuffer(orderNumber);
 
     const page = pdfDoc.addPage([940, 544]);
 
     //set 6mm bleed
     page.setBleedBox(0, 0, 18, 18)
 
-    const barcodeImageProd = await pdfDoc.embedPng(barcodeProdData);
-    const barcodeImageOrder = await pdfDoc.embedPng(barcodeOrderData);
+    try {
+        const barcodeImageProd = await pdfDoc.embedPng(barcodeProdData);
+        const barcodeImageOrder = await pdfDoc.embedPng(barcodeOrderData);
 
-    //draw product number barcode
-    page.drawImage(barcodeImageProd, {
-        width: pdfConstants.barcodeWidth,
-        height: pdfConstants.barcodeHeight,
-        x: pdfConstants.barcodeX,
-        y: 544 / 2,
-    });
+        //draw product number barcode
+        page.drawImage(barcodeImageProd, {
+            width: pdfConstants.barcodeWidth,
+            height: pdfConstants.barcodeHeight,
+            x: pdfConstants.barcodeX,
+            y: 544 / 2,
+        });
 
-    page.drawImage(barcodeImageOrder, {
-        width: pdfConstants.barcodeWidth,
-        height: pdfConstants.barcodeHeight,
-        x: pdfConstants.barcodeX,
-        y: 544 / 4,
-    });
+        page.drawImage(barcodeImageOrder, {
+            width: pdfConstants.barcodeWidth,
+            height: pdfConstants.barcodeHeight,
+            x: pdfConstants.barcodeX,
+            y: 544 / 4,
+        });
 
-    await pdfDoc.save();
+        await pdfDoc.save();
+    } catch (e) {
+        logger.error(`Error generating barcode page: ${e}`);
+    }
+
 }
 
 module.exports = {
