f11e84dd30505a4a2854b5f3e4bfb90e4607cbd6
chavocito <102731139+nx-francisbota@users.noreply.github.com>
2024-03-21 12:29:48 +0000

    update bash script

--- run_ftp_endpoint.sh
+++ run_ftp_endpoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+source .env
+echo "Environment variable file detected. Reading variables"
+
 SERVER_HOST=${HOST}
 SERVER_PORT=${PORT}
 
+if [[ ! -z "$SERVER_HOST" && ! -z "$SERVER_PORT" ]]; then
+  echo "SERVER_HOST and SERVER_PORT found"
+else
+  echo "SERVER_HOST is not defined"
+fi
+
 SERVER_URL="$SERVER_HOST:$SERVER_PORT/ftp"
+
+echo "Server URL: $SERVER_URL"
 
 # Make a GET request to the /ftp route to trigger the process
 curl -X GET "$SERVER_URL"
